f88cf3770354cc6a5ae10898e11e7e7f083de1c0
mhceg791-199 <mhceg791@gmail.com>
2026-02-21 14:27:56 +0200

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://localhost:5000",
-        target: "https://chess-backend-nine.vercel.app",
+        target: "https://chess-backend-nine.vercel.app/api",
         changeOrigin: true,
       },
     },
